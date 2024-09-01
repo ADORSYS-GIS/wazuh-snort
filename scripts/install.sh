848bb9746697d024b5284969309e0efc9f9bb0b7
Dylane Bengono <chaneldylanebengono@gmail.com>
2024-09-01 16:11:44 +0100

    Refactor Snort package installation process for Linux

--- scripts/install.sh
+++ scripts/install.sh
@@ -86,124 +86,55 @@ install_snort_macos() {
     create_snort_dirs_files /usr/local/etc/rules /usr/local/etc/so_rules /usr/local/etc/lists /var/log/snort
     create_snort_files /usr/local/etc/rules/local.rules /usr/local/etc/lists/default.blocklist
 
-    echo 'alert icmp any any -> any any ( msg:"ICMP Traffic Detected"; sid:10000001; metadata:policy security-ips alert; )' | maybe_sudo tee /usr/local/etc/rules/local.rules >/dev/null
+    echo 'alert icmp any any -> any any ( msg:"ICMP Traffic Detected"; sid:10000001; metadata:policy security-ips alert; )' | maybe_sudo tee /usr/local/etc/rules/local.rules > /dev/null
 
     configure_snort_logging_macos
     update_ossec_conf_macos
     start_snort_macos
-
-    # Change ownership and set capabilities
-    maybe_sudo chown "$USER" /usr/local/bin/snort
-    maybe_sudo chmod u+s /usr/local/bin/snort
 }
 
 # Function to install Snort on Linux
 install_snort_linux() {
-    # Disable interactive prompts
-    export DEBIAN_FRONTEND=noninteractive
     print_step "Installing" "Snort for Linux"
-    maybe_sudo apt-get update
-    maybe_sudo apt install net-tools -y
-
-    # Determine the system architecture
-    ARCH=$(dpkg --print-architecture)
-
-    # Define the URL of the archive based on the architecture
-    if [[ "$ARCH" == "amd64" ]]; then
-        URL="https://github.com/ADORSYS-GIS/wazuh-snort/releases/download/fix-scripts-tests/snort3-packages-amd64.zip" # to be updated
-    elif [[ "$ARCH" == "arm64" ]]; then
-        URL="https://github.com/ADORSYS-GIS/wazuh-snort/releases/download/fix-scripts-tests/snort3-packages-arm64.zip" # to be updated
-    else
-        error_message "Unsupported architecture: $ARCH"
-        exit 1
-    fi
-
-    # Install required packages if not already installed
-    if ! command -v unzip &>/dev/null; then
-        echo "unzip could not be found. Installing unzip..."
-        sudo apt-get update && sudo apt-get install -y unzip
-    fi
-
-    # Create a temporary directory
-    TEMP_DIR=$(mktemp -d)
-
-    # Download the archive to the temporary directory
-    curl -L $URL -o $TEMP_DIR/snort3-packages.zip
-
-    # Extract the archive
-    unzip $TEMP_DIR/snort3-packages.zip -d $TEMP_DIR
-
-    # Make the .deb files executable
-    maybe_sudo chmod +x $TEMP_DIR/*.deb
-
-    # Install all .deb files
-    maybe_sudo dpkg -i $TEMP_DIR/*.deb
-
-    # Check if /usr/local/lib is in the library path
-    if ! grep -q "/usr/local/lib" /etc/ld.so.conf.d/*; then
-        echo "/usr/local/lib" | sudo tee /etc/ld.so.conf.d/local-lib.conf
-    fi
-
-    # Update the linker cache
-    maybe_sudo ldconfig
-
-    # Clean up the temporary directory
-    rm -rf $TEMP_DIR
-
-    success_message "Installation completed for architecture $ARCH"
 
     # Get the default network interface
     INTERFACE=$(ip route | grep default | awk '{print $5}')
+    HOMENET=$(ip -4 addr show $INTERFACE | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 
-    # Check if the interface is found
-    if [ -z "$INTERFACE" ]; then
-        error_message "No network interface found."
-        exit 1
-    fi
+    # Function to install Snort on Linux
+    install_snort_apt() {
+        sudo DEBIAN_FRONTEND=noninteractive apt-get update
+        sudo DEBIAN_FRONTEND=noninteractive apt-get install snort -y --no-install-recommends
+    }
 
-    # Create the snort system user
-    maybe_sudo useradd -r -s /usr/sbin/nologin -M -c SNORT_IDS snort
+    # Install Snort
+    install_snort_apt
 
-    # Grant permissions to the log directory
-    maybe_sudo chmod -R 5775 /var/log/snort
-    maybe_sudo chown -R snort:snort /var/log/snort
+    # Function to configure Snort to use the main network interface and set HomeNet
+    configure_snort_interface_and_homenet() {
+        if [ ! -f /etc/snort/snort.conf ]; then
+            # Create snort.conf with minimal configuration
+            echo "ipvar HOME_NET $HOMENET/24" | sudo tee /etc/snort/snort.conf
+            echo "config interface: $INTERFACE" | sudo tee -a /etc/snort/snort.conf
+        else
+            # Update existing snort.conf
+            maybe_sudo sed -i "s/^ipvar HOME_NET .*/ipvar HOME_NET $HOMENET\/24/" /etc/snort/snort.conf
+            maybe_sudo sed -i "s/^config interface: .*/config interface: $INTERFACE/" /etc/snort/snort.conf
+        fi
+    }
 
-    # Create the systemd service file
-maybe_sudo cat > /etc/systemd/system/snort3.service << EOL
-[Unit]
-Description=Snort3 NIDS Daemon
-After=syslog.target network.target
+    # Run the configuration function
+    configure_snort_interface_and_homenet
 
-[Service]
-Type=simple
-ExecStart=/usr/local/bin/snort -c /usr/local/etc/snort/snort.lua -s 65535 -k none -l /var/log/snort -D -u snort -g snort -i $INTERFACE -m 0x1b --create-pidfile
-ExecStop=/bin/kill -9 \$MAINPID
-
-[Install]
-WantedBy=multi-user.target
-EOL
-
-    # Enable and start the service
-    maybe_sudo systemctl enable snort3
-    #sudo systemctl start snort3
-    maybe_sudo service snort3 start
-    # Check the status of the service
-    maybe_sudo service snort3 status
-
-    # Check the status of the service
-    if systemctl is-active --quiet snort3.service; then
-        success_message "Service 'snort3' is running."
-    else
-        error_message "Service 'snort3' is not running."
-    fi
-
-    # Show detailed status of the service
-    systemctl status snort3.service
+    # Restart Snort service
+    maybe_sudo systemctl restart snort || {
+        echo "Failed to restart Snort. Check the configuration."
+        maybe_sudo systemctl status snort
+    }
 
     configure_snort_linux
-    #update_ossec_conf_linux
-    #start_snort_linux
-
+    update_ossec_conf_linux
+    start_snort_linux
 }
 
 # Function to configure Snort logging on macOS
@@ -213,7 +144,7 @@ configure_snort_logging_macos() {
 
     info_message "Configuring Snort logging"
     if ! grep -q "$content_to_add" "$config_file"; then
-        echo -e "$content_to_add" | maybe_sudo tee -a "$config_file" >/dev/null
+        echo -e "$content_to_add" | maybe_sudo tee -a "$config_file" > /dev/null
         success_message "Snort logging configured in $config_file"
     else
         info_message "Snort logging is already configured in $config_file"
@@ -248,72 +179,20 @@ start_snort_macos() {
 # Function to configure Snort on Linux
 configure_snort_linux() {
     info_message "Configuring Snort"
+    maybe_sudo sed -i 's/output alert_fast: snort.alert.fast/output alert_fast: snort.alert/g' /etc/snort/snort.conf
+    maybe_sudo sed -i 's/# output alert_syslog: LOG_AUTH LOG_ALERT/output alert_syslog: LOG_AUTH LOG_ALERT/g' /etc/snort/snort.conf
+    echo 'alert icmp any any -> any any (msg:"ICMP connection attempt:"; sid:1000010; rev:1;)' | maybe_sudo tee -a /etc/snort/rules/local.rules > /dev/null
 
-    # Ensure INTERFACE is set
-    if [ -z "$INTERFACE" ]; then
-        echo "Error: INTERFACE variable is not set."
-        exit 1
+    info_message "Downloading and configuring Snort rule files"
+    maybe_sudo curl -SL --progress-bar -o community-rules.tar.gz https://www.snort.org/downloads/community/community-rules.tar.gz
+    maybe_sudo tar -xvzf community-rules.tar.gz -C /etc/snort/rules --strip-components=1
+    maybe_sudo rm community-rules.tar.gz
+
+    if ! grep -q "include \$RULE_PATH/community.rules" /etc/snort/snort.conf; then
+        echo "include \$RULE_PATH/community.rules" | maybe_sudo tee -a /etc/snort/snort.conf
+        success_message "Snort rule files configured on Linux"
     fi
-
-    # Directories to check and create
-    directories=("/usr/local/etc/rules" "/usr/local/etc/so_rules" "/usr/local/etc/lists" "/var/log/snort")
-
-    # Files to check and create
-    files=("/usr/local/etc/rules/local.rules" "/usr/local/etc/lists/default.blocklist")
-
-    # Check and create directories if necessary
-    create_dirs_files "${directories[@]}"
-
-    # Check and create files if necessary
-    create_files "${files[@]}"
-
-    # Add the rule if it does not already exist
-    RULE="alert icmp any any -> any any ( msg:\"ICMP Traffic Detected\"; sid:10000001; metadata:policy security-ips alert; )"
-    grep -qF "$RULE" /usr/local/etc/rules/local.rules || echo "$RULE" >> /usr/local/etc/rules/local.rules
-
-    # Get the IP address of the default network interface
-    HOME_NET=$(ip -o -f inet addr show "$INTERFACE" | awk '{print $4}')
-
-    # Check if HOME_NET is obtained
-    if [ -z "$HOME_NET" ]; then
-        echo "Error: Unable to get IP address for interface $INTERFACE."
-        exit 1
-    fi
-
-    # Download and extract Snort 3 community rules
-    cd /usr/local/etc/rules || exit 1
-    wget https://www.snort.org/downloads/community/snort3-community-rules.tar.gz || exit 1
-    tar xzf snort3-community-rules.tar.gz || exit 1
-
-    # Verify the extraction
-    echo "Listing extracted files:"
-    ls -l snort3-community-rules
-
-    # Path to the snort.lua file
-    SNORT_LUA="/usr/local/etc/snort/snort.lua"
-
-    # Check if the snort.lua file exists
-    if [ ! -f "$SNORT_LUA" ]; then
-        echo "Error: $SNORT_LUA file not found."
-        exit 1
-    fi
-
-    # Update the ips section to enable decoder and inspector alerts, include local and community rules
-    sed -i '/ips = {/,/variables = default_variables/ s/^--\(enable_builtin_rules\s*=\s*true\)/\1/' "$SNORT_LUA" || exit 1
-    sed -i '/ips = {/,/variables = default_variables/ s/^--\(include\s*=\s*RULE_PATH\s*\.\.\s*\"\/local\.rules\"\)/\1/' "$SNORT_LUA" || exit 1
-    sed -i '/ips = {/,/variables = default_variables/ s/^--\(include\s*=\s*RULE_PATH\s*\.\.\s*\"\/snort3-community-rules\/snort3-community\.rules\"\)/\1/' "$SNORT_LUA" || exit 1
-
-    # Ensure the HOME_NET and EXTERNAL_NET variables are set dynamically
-    sed -i "s|HOME_NET = .*$|HOME_NET = \"$HOME_NET\"|" "$SNORT_LUA" || exit 1
-    sed -i 's|EXTERNAL_NET = .*$|EXTERNAL_NET = "!$HOME_NET"|' "$SNORT_LUA" || exit 1
-
-    # Notify the user of success
-    echo "Successfully configured Snort with community rules and updated HOME_NET to $HOME_NET and EXTERNAL_NET variables in $SNORT_LUA."
-
-    success_message "Snort configured on Linux"
 }
-
-
 
 # Function to update ossec.conf on Linux
 update_ossec_conf_linux() {
@@ -330,9 +209,8 @@ update_ossec_conf_linux() {
 # Function to start Snort on Linux
 start_snort_linux() {
     info_message "Restarting Snort"
-# Run Snort
-snort -c /usr/local/etc/snort/snort.lua -R /usr/local/etc/rules/local.rules
-snort -c /usr/local/etc/snort/snort.lua -R /usr/local/etc/rules/local.rules -i "$INTERFACE" -A alert_fast -s 65535 -k none 
+    maybe_sudo systemctl restart snort
+    maybe_sudo snort -q -c /etc/snort/snort.conf -l /var/log/snort -A fast &
     success_message "Snort started on Linux"
 }
 
@@ -353,18 +231,19 @@ maybe_sudo() {
 # Main function to install and configure Snort
 install_snort() {
     case "$OSTYPE" in
-    darwin*)
-        install_snort_macos
-        ;;
-    linux-gnu*)
-        install_snort_linux
-        ;;
-    *)
-        error_message "Unsupported OS type: $OSTYPE"
-        exit 1
-        ;;
+        darwin*)
+            install_snort_macos
+            ;;
+        linux-gnu*)
+            install_snort_linux
+            ;;
+        *)
+            error_message "Unsupported OS type: $OSTYPE"
+            exit 1
+            ;;
     esac
 }
 
 # Run the main installation function
 install_snort
+
