65dff3513b0bc34e8bf5c43a4fa5ba5db8e95a57
Dylane Bengono <chaneldylanebengono@gmail.com>
2024-09-03 12:55:55 +0100

    Refactor Snort installation process for Linux and update pytest

--- scripts/tests/test_install.py
+++ scripts/tests/test_install.py
@@ -52,14 +52,14 @@ def test_snort_default_interface_configured(host):
 
 def test_home_net_configured_in_snort(host):
     """Test if HOME_NET is correctly configured in snort.conf."""
-    interface = host.interface.default
+    interface = host.interface("default")
     home_net = host.check_output(f"ip -o -f inet addr show {interface.name} | awk '/scope global/ {{print $4}}'")
     snort_conf = host.file("/etc/snort/snort.conf")
     assert f"ipvar HOME_NET [{home_net}]" in snort_conf.content_string, f"HOME_NET should be configured as {home_net} in snort.conf"
 
 def test_ossec_conf_updated_with_snort(host):
     """Test if ossec.conf is updated with Snort logging configuration."""
-    ossec_conf = host.file(OSSEC_CONF_PATH)
+    ossec_conf = host.file("/var/ossec/etc/ossec.conf")
     snort_log_config = """
         <!-- snort -->
         <localfile>
