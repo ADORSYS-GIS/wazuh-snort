b37230789036007939e7a69a148b8072261ef4b9
Dylane Bengono <chaneldylanebengono@gmail.com>
2024-09-03 21:08:12 +0100

    refactor: Update snort.conf HOME_NET configuration in test_install.py

--- scripts/tests/test_install.py
+++ scripts/tests/test_install.py
@@ -41,20 +41,20 @@ def test_snort_default_interface_configured(host):
     ), f"Interface {interface.name} should be configured in snort.conf"
 
 
-    def test_configure_snort_homenet(host):
-        """Test if Snort is configured to set HomeNet."""
-        interface = host.run("ip route | grep default | awk '{print $5}'").stdout.strip()
-        homenet = host.run(f"ip -4 addr show {interface} | grep -oP '(?<=inet\s)\d+(\.\d+){3}'").stdout.strip()
+def test_configure_snort_homenet(host):
+    """Test if Snort is configured to set HomeNet."""
+    interface = host.run("ip route | grep default | awk '{print $5}'").stdout.strip()
+    homenet = host.run(f"ip -4 addr show {interface} | grep -oP r'(?<=inet\s)\d+(\.\d+){3}'").stdout.strip()
 
-        snort_conf = host.file("/etc/snort/snort.conf")
-        if not snort_conf.exists:
-            # Create snort.conf with minimal configuration
-            expected_content = f"ipvar HOME_NET {homenet}/24"
-        else:
-            # Update existing snort.conf
-            expected_content = f"ipvar HOME_NET {homenet}/24"
+    snort_conf = host.file("/etc/snort/snort.conf")
+    if not snort_conf.exists:
+        # Create snort.conf with minimal configuration
+        expected_content = f"ipvar HOME_NET {homenet}/24"
+    else:
+        # Update existing snort.conf
+        expected_content = f"ipvar HOME_NET {homenet}/24"
 
-        assert expected_content.strip() in snort_conf.content_string.strip(), "Snort should be configured to set HomeNet"
+    assert expected_content.strip() in snort_conf.content_string.strip(), "Snort should be configured to set HomeNet"
 
 
 def test_update_ossec_conf_linux(host):
