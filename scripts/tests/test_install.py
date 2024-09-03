b701310f9b3cf876418c7105b72b13cc90a41b04
Dylane Bengono <chaneldylanebengono@gmail.com>
2024-09-03 23:00:32 +0100

    refactor: Update snort.conf HOME_NET configuration in test_install.py

--- scripts/tests/test_install.py
+++ scripts/tests/test_install.py
@@ -40,15 +40,20 @@ def test_default_network_interface(host):
 def test_home_network_ip(host):
     # Check if the HOME_NET is correctly set in the configuration file
     interface = host.run("ip route | grep default | awk '{print $5}'").stdout.strip()
-    homenet = host.run(f"ip -4 addr show {interface} | grep -oP '(?<=inet\s)\d+(\.\d+){3}'").stdout.strip()
+    homenet = host.run(f"ip -4 addr show {interface} | grep -oP r'(?<=inet\s)\d+(\.\d+){3}'").stdout.strip()
     conf_file = host.file("/etc/snort/snort.conf")
+    print(f"Home Network IP: {homenet}")
+    print(f"Configuration File Content:\n{conf_file.content_string}")
     assert conf_file.contains(f"ipvar HOME_NET {homenet}/24")
 
 def test_snort_configuration_file(host):
     # Verify the Snort configuration file contains the correct settings
     conf_file = host.file("/etc/snort/snort.conf")
     interface = host.run("ip route | grep default | awk '{print $5}'").stdout.strip()
+    print(f"Network Interface: {interface}")
+    print(f"Configuration File Content:\n{conf_file.content_string}")
     assert conf_file.contains(f"config interface: {interface}")
+
 
 def test_update_ossec_conf_linux(host):
     """Test if ossec.conf is updated on Linux."""
