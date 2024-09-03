faba06e67ba36686cf94bb726b3338bbc07c7e8f
Dylane Bengono <chaneldylanebengono@gmail.com>
2024-09-03 19:59:57 +0100

    feat: Update snort.conf with correct HOME_NET configuration
    The code changes in `test_install.py` ensure that the `HOME_NET` variable in `snort.conf` is correctly configured. The previous implementation used the `ip` command with the `-o` and `-f inet` options to retrieve the IP address, but it didn't work as expected. The updated code now uses the `ip` command with the `-4` option and `grep` to extract the IP address. The `HOME_NET` variable is then updated in `snort.conf` to include the IP address with a `/24` subnet mask.
    
    This commit message follows the established conventions in the repository.

--- scripts/tests/test_install.py
+++ scripts/tests/test_install.py
@@ -37,9 +37,9 @@ def test_snort_default_interface_configured(host):
 def test_home_net_configured_in_snort(host):
     """Test if HOME_NET is correctly configured in snort.conf."""
     interface = host.interface("default")
-    home_net = host.check_output(f"ip -o -f inet addr show {interface.name} | awk '/scope global/ {{print $4}}'")
+    home_net = host.check_output(f"ip -4 addr show {interface.name} | grep -oP '(?<=inet\s)\d+(\.\d+){3}'")
     snort_conf = host.file("/etc/snort/snort.conf")
-    assert f"ipvar HOME_NET [{home_net}]" in snort_conf.content_string, f"HOME_NET should be configured as {home_net} in snort.conf"
+    assert f"ipvar HOME_NET {home_net}/24" in snort_conf.content_string, f"HOME_NET should be configured as {home_net}/24 in snort.conf"
 
 def test_ossec_conf_updated_with_snort(host):
     """Test if ossec.conf is updated with Snort logging configuration."""
