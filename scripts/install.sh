54dade7aa3745b4de7b0684c1d406640514ac407
Dylane Bengono <chaneldylanebengono@gmail.com>
2024-09-11 13:32:03 +0100

    chore: Update Snort installation script for macOS

--- scripts/install.sh
+++ scripts/install.sh
@@ -81,7 +81,7 @@ create_snort_files() {
 # Function to install Snort on macOS
 install_snort_macos() {
     print_step "Installing" "Snort for macOS"
-    maybe_sudo brew install snort
+    brew install snort
 
     create_snort_dirs_files /usr/local/etc/rules /usr/local/etc/so_rules /usr/local/etc/lists /var/log/snort
     create_snort_files /usr/local/etc/rules/local.rules /usr/local/etc/lists/default.blocklist
