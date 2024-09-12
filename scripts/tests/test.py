04f50141b60ec8d2144d8e2dc791a0f1d59c3655
Dylane Bengono <chaneldylanebengono@gmail.com>
2024-09-12 15:17:31 +0100

    chore: Update Snort installation script for macOS and Linux

--- scripts/tests/test.py
+++ scripts/tests/test.py
@@ -1,8 +1,6 @@
 import pytest
 import testinfra
 
-
-
 @pytest.fixture(scope="module")
 def install_dependencies(host):
     """Install dependencies and run the install script."""
@@ -16,6 +14,9 @@ def install_dependencies(host):
         host.run("apk update")
         host.run("apk add curl gnupg2 iproute2")
 
+    elif os == "darwin":  # macOS
+        host.run("brew install curl gnupg2 iproute2")
+
     else:
         pytest.fail("Unsupported OS for dependency installation")
 
@@ -28,37 +29,50 @@ def test_snort_is_installed(host):
 
 
 def test_snort_conf_file_exists(host):
-    """Test if snort.conf file exists."""
-    snort_conf = host.file("/etc/snort/snort.conf")
+    """Test if snort configuration file exists."""
+    os = host.system_info.distribution
+    if os == "darwin":  # macOS
+        snort_conf = host.file("/usr/local/etc/snort/snort.lua")
+    else:
+        snort_conf = host.file("/etc/snort/snort.conf")
+        
     assert snort_conf.exists, "snort.conf file should exist"
 
 
 def test_snort_interface_configuration(host):
-    # Retrieve the default network interface
+    """Test if the network interface is correctly configured in the Snort configuration file."""
+    os = host.system_info.distribution
     interface = host.run("ip route | grep default | awk '{print $5}'").stdout.strip()
     
-    # Check if the interface is present in the snort.debian.conf file
-    snort_conf = host.file("/etc/snort/snort.debian.conf")
-    assert interface in snort_conf.content_string, "Interface should be present in snort.debian.conf"
-
+    if os == "darwin":  # macOS
+        snort_conf = host.file("/usr/local/etc/snort/snort.lua")
+    else:
+        snort_conf = host.file("/etc/snort/snort.debian.conf")
+    
+    assert interface in snort_conf.content_string, "Interface should be present in Snort configuration file"
 
 
 def test_update_ossec_conf_linux(host):
     """Test if ossec.conf is updated on Linux."""
-    ossec_conf_path = "/var/ossec/etc/ossec.conf"
-    expected_content = """
-        <!-- snort -->
-        <localfile>
-            <log_format>snort-full</log_format>
-            <location>/var/log/snort/snort.alert.fast</location>
-        """
-
+    os = host.system_info.distribution
+    if os == "darwin":  # macOS
+        ossec_conf_path = "/usr/local/etc/ossec.conf"
+        expected_content = """
+            <!-- snort -->
+            <localfile>
+                <log_format>snort-full</log_format>
+                <location>/usr/local/var/log/snort/snort.alert.fast</location>
+            """
+    else:
+        ossec_conf_path = "/var/ossec/etc/ossec.conf"
+        expected_content = """
+            <!-- snort -->
+            <localfile>
+                <log_format>snort-full</log_format>
+                <location>/var/log/snort/snort.alert.fast</location>
+            """
+    
     ossec_conf = host.file(ossec_conf_path)
     assert (
         expected_content.strip() in ossec_conf.content_string.strip()
-    ), "ossec.conf should be updated on Linux"
-
-
-
-
-    
+    ), "ossec.conf should be updated accordingly"
