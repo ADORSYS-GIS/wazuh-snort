909488385d741dc26e317895126ce7eb57ae61e8
Calebasah <asahcaleb4@gmail.com>
2025-01-20 17:51:34 +0100

    Fix: setup configuration for snort on linux

--- scripts/install.sh
+++ scripts/install.sh
@@ -363,9 +363,6 @@ configure_snort_linux() {
     sed_alternative -i 's/# output alert_syslog: LOG_AUTH LOG_ALERT/output alert_syslog: LOG_AUTH LOG_ALERT/g' /etc/snort/snort.conf
     if [! -f /etc/snort/rules/local.rules]; then
         maybe_sudo curl -o /etc/snort/rules/local.rules https://raw.githubusercontent.com/ADORSYS-GIS/wazuh-snort/refs/heads/main/scripts/windows/local.rules
-    else
-        error_message "File /etc/snort/rules/local.rules does not exist"
-        exit 1 
     fi
     echo 'alert icmp any any -> any any (msg:"ICMP connection attempt:"; sid:1000010; rev:1;)' | maybe_sudo tee -a /etc/snort/rules/local.rules > /dev/null
 
